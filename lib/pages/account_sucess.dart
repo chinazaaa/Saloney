48ee8509af648720596022b9cbb76f1c9135410c
Chinaza <obiekweagnesmary@gmail.com>
2020-09-07 14:39:46 +0100

    success screens

--- lib/pages/account_sucess.dart
+++ lib/pages/account_sucess.dart
@@ -3,98 +3,70 @@ class AccountSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black,),
-        ),
-      ),
-      body: SafeArea(
-              child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.topLeft,  
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        
-                           
-                            Text(
-                              "Account created successfully",
-                              style: TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                        SizedBox(
-                          height: 300,
-                        ),
-                      
-                            Text(
-                              "We just sent you a verification email",
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey[700]),
-                            ),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      // child: Column(
-                      //   children: <Widget>[
-                      //     makeInput(hint: "Email Address"),
-                          
-                      //     Container(
-                      //         child: Row(
-                      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //       children: <Widget>[
-                      //         ],
-                      //     ))
-                      //   ],
-                      // ),
-                    ),
-                         Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Container(
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        height: 60,
-                        padding: EdgeInsets.only(top: 3, left: 3),
-                        decoration: BoxDecoration(
-                color: Color(0xfff3236e),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Text(
-                "Get Started",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Colors.white),
-              ),
-                      ),
-                    ),
-                  
-                  
-                  ],
-                ),
-              ),
-            
-            ],
+        appBar: AppBar(
+          elevation: 0,
+          brightness: Brightness.light,
+          toolbarHeight: 100,
+          title: Text(
+            "Account created \n successfully",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
           ),
         ),
-      ));
-    
+        body: SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 50.0),
+                    child: Image.asset(
+                       "assets/account_success.png"),
+                    height: 150,
+                  ),
+                  Text(
+                    "We just sent you a verification email",
+                    style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      height: 60,
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration: BoxDecoration(
+                        color: Color(0xfff3236e),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ]),
+          ),
+        ));
   }
+
 }
--- lib/pages/reset_success.dart
+++ lib/pages/reset_success.dart
@@ -1,124 +1,69 @@
 import 'package:flutter/material.dart';
-class ResetSuccess extends StatelessWidget {
+class AccountSuccess extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        brightness: Brightness.light,
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios, size: 20, color: Colors.black,),
+        appBar: AppBar(
+          elevation: 0,
+          brightness: Brightness.light,
+          toolbarHeight: 100,
+          title: Text(
+            "Reset Complete",
+            style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
+          ),
         ),
-      ),
-      body: SafeArea(
-              child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Container(
-                alignment: Alignment.topLeft,  
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        
-                           
-                            Text(
-                              "Reset Complete",
-                              style: TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                      
-                            Text(
-                              "Your password has been changed",
-                              style: TextStyle(
-                                  fontSize: 15, color: Colors.grey[700]),
-                            ),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      // child: Column(
-                      //   children: <Widget>[
-                      //     makeInput(hint: "Email Address"),
-                          
-                      //     Container(
-                      //         child: Row(
-                      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //       children: <Widget>[
-                      //         ],
-                      //     ))
-                      //   ],
-                      // ),
-                    ),
-                         Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Container(
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        height: 60,
-                        padding: EdgeInsets.only(top: 3, left: 3),
-                        decoration: BoxDecoration(
-                color: Color(0xfff3236e),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              child: Text(
-                "Continue",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                    color: Colors.white),
-              ),
+        body: SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: double.infinity,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 50.0),
+                    child: Image.asset(
+                       "assets/reset_success.png"),
+                    height: 150,
+                  ),
+                  
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      height: 60,
+                      padding: EdgeInsets.only(top: 3, left: 3),
+                      decoration: BoxDecoration(
+                        color: Color(0xfff3236e),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        "Continue",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                            color: Colors.white),
                       ),
                     ),
-                  
-                  
-                  ],
-                ),
-              ),
-            
-            ],
+                  ),
+                ]),
           ),
-        ),
-      ));
-    
+        ));
   }
 
-  Widget makeInput({obscureText = false, String hint}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-    
-        
-        TextField(
-          obscureText: obscureText,
-          decoration: InputDecoration(
-            hintText: hint, 
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[400])
-            ),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey[400])
-            ),
-          ),
-        ),
-        SizedBox(height: 30,),
-      ],
-    );
-}
 }
