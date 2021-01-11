87d31d1a8334179a878ed88c1f026261cfbb0b3d
Chukwuebuka Onah <revelationjay02@gmail.com>
2021-01-11 10:04:24 +0100

    Changed Native Screen for android and iOS

--- android/app/src/main/kotlin/com/example/starter_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/starter_project/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.starter_project
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
