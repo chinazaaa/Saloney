5349a394e6bb262ceaf7096ced37e41b5941f474
TImothy Odebunmi <dev.brymes@gmail.com>
2021-05-13 14:35:21 +0100

    Unfinished API endpoints

--- lib/services/http_service_brymes/api.dart
+++ lib/services/http_service_brymes/api.dart
@@ -2,14 +2,16 @@ import 'package:starter_project/index.dart';
 
 abstract class Api {
   // Future<void> logIn({String username, String password});
-  Future<ApiResponse> updateCustomerProfile(
-      String username, phoneNumber, address);
-  Future<ApiResponse> updateSalonOwnerProfile();
+  // Future<ApiResponse> updateCustomerProfile(
+  //     String username, phoneNumber, address);
+  // Future<ApiResponse> updateSalonOwnerProfile();
 
-  Future<ApiResponse> updateSalonProfile(
-    String nameOfSalon,
-    description,
-    location,
-    //FIXME
-  );
+  // Future<ApiResponse> updateSalonProfile(
+  //   String nameOfSalon,
+  //   description,
+  //   location,
+  //   //FIXME
+  // );
+  Future<ApiResponse> createService(
+      {String service, String description, String category, String price});
 }
