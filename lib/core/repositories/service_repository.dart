b117dd18b384eff3fbb70fffc7648e5e407ce556
TImothy Odebunmi <dev.brymes@gmail.com>
2021-05-13 14:17:47 +0100

    Repository for Service Endpoints
    Create Service Successfully implemented

---
+++ lib/core/repositories/service_repository.dart
@@ -0,0 +1,62 @@
+import 'package:starter_project/index.dart';
+import 'package:starter_project/models/service/serviceResponses.dart';
+import 'package:starter_project/core/api/api_utils/network_exceptions.dart';
+
+class ServiceRepo extends BaseNotifier {
+  // API
+  Api _api = Get.find();
+
+  Future<bool> addService({
+    String service,
+    description,
+    price,
+    category,
+    // bool isPublished
+  }) async {
+    setState(ViewState.Busy);
+
+    // List serviceList = [];
+
+    CreateServiceResponse resp;
+    try {
+      resp = await _api.createService(
+        service: service,
+        description: description,
+        price: price,
+        category: category,
+        // isPublished: isPublished
+      );
+
+      setState(ViewState.Idle);
+
+      if (resp.success) {
+        //Cache login info
+        print(resp.data);
+
+        return true;
+      } else {
+        Get.snackbar(
+          'Error',
+          '${resp.message}',
+          margin: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+          snackStyle: SnackStyle.FLOATING,
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.black26,
+        );
+        return false;
+      }
+    } on NetworkException {
+      Get.snackbar(
+        'No Internet!',
+        'Check Internet Connection and try again',
+        margin: EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+        snackStyle: SnackStyle.FLOATING,
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.black26,
+      );
+    }
+
+    setState(ViewState.Idle);
+    return false;
+  }
+}
