53b70545049e7134702209520dbd5bb8d0cd833b
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2021-06-18 16:55:06 +0100

    published and unpublished screens

--- lib/Salon/pages/screens/published_service.dart
+++ lib/Salon/pages/screens/published_service.dart
@@ -1,10 +1,73 @@
-// FIXME northing here
-// import 'package:flutter/material.dart';
-// class PublishedService extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-    
-//     );
-//   }
-// }
+import 'package:flutter/material.dart';
+
+class PublishedService extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: DataTable(
+          showBottomBorder: true,
+          dataRowHeight: 60,
+          headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
+          columns: <DataColumn>[
+            DataColumn(
+              label: Text('Service Name'),
+            ),
+            DataColumn(
+              label: Text('Image'),
+            ),
+            DataColumn(
+              label: Text('Actions'),
+            ),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text('Hair')),
+              DataCell(Image.network(
+                'https://res.cloudinary.com/saloney/image/upload/v1623862747/SaloneyLogo.png',
+              )),
+              DataCell(
+                PopupOptionMenu(),
+              )
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise'))
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise'))
+            ])
+          ]),
+    );
+  }
+}
+enum MenuOption {Edit,Unpublish, Delete}
+class PopupOptionMenu extends StatelessWidget {
+  const PopupOptionMenu({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<MenuOption>(
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuEntry<MenuOption>>[
+          PopupMenuItem(
+            child: Icon(Icons.edit, color: Colors.black, size: 28.0),
+            //child: Text("Edit"),
+          value: MenuOption.Edit,
+          ),
+            PopupMenuItem(
+                child: Icon(Icons.cancel, color: Colors.black, size: 28.0),
+              //child: Text("Publish"),
+          value: MenuOption.Unpublish,
+          ),
+            PopupMenuItem(
+                child: Icon(Icons.delete,color: Colors.black, size: 28.0),
+              //child: Text("Delete"),
+          value: MenuOption.Delete,
+          )
+        ];
+      },
+    );
+  }
+}
--- lib/Salon/pages/screens/unpublished_service.dart
+++ lib/Salon/pages/screens/unpublished_service.dart
@@ -1,22 +1,73 @@
-// import 'package:flutter/material.dart';
-// class UnPublishedService extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: DataTable(
-//         showBottomBorder: true,
-//         dataRowHeight: 60,
-//         headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
-//         columns: <DataColumn> [
-//           DataColumn(label: Text('Service Name'),),
-//          DataColumn(label: Text('Image'),),
-//           DataColumn(label: Text('Actions'),),
-//         ],
-//         // rows = _serviceDetails().
-//         ),
-//     );
+import 'package:flutter/material.dart';
 
- 
-//   }
-
-// }
+class UnPublishedService extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: DataTable(
+          showBottomBorder: true,
+          dataRowHeight: 60,
+          headingRowColor: MaterialStateProperty.all(Colors.grey[200]),
+          columns: <DataColumn>[
+            DataColumn(
+              label: Text('Service Name'),
+            ),
+            DataColumn(
+              label: Text('Image'),
+            ),
+            DataColumn(
+              label: Text('Actions'),
+            ),
+          ],
+          rows: [
+            DataRow(cells: [
+              DataCell(Text('Hair')),
+              DataCell(Image.network(
+                'https://res.cloudinary.com/saloney/image/upload/v1623862747/SaloneyLogo.png',
+              )),
+              DataCell(
+                PopupOptionMenu(),
+              )
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise'))
+            ]),
+            DataRow(cells: [
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise')),
+              DataCell(Text('Cruise'))
+            ])
+          ]),
+    );
+  }
+}
+enum MenuOption {Edit, Publish, Delete}
+class PopupOptionMenu extends StatelessWidget {
+  const PopupOptionMenu({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<MenuOption>(
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuEntry<MenuOption>>[
+          PopupMenuItem(
+            child: Icon(Icons.edit, color: Colors.black, size: 28.0),
+            //child: Text("Edit"),
+          value: MenuOption.Edit,
+          ),
+            PopupMenuItem(
+                child: Icon(Icons.publish, color: Colors.black, size: 28.0),
+              //child: Text("Publish"),
+          value: MenuOption.Publish,
+          ),
+            PopupMenuItem(
+                child: Icon(Icons.delete,color: Colors.black, size: 28.0),
+              //child: Text("Delete"),
+          value: MenuOption.Delete,
+          )
+        ];
+      },
+    );
+  }
+}
