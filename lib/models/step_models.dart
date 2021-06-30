2e966fa146bef48e65db82143886bf12f05b6048
chinazaaa <50022245+chinazaaa@users.noreply.github.com>
2021-06-30 13:46:05 +0100

    Update step_models.dart

--- lib/models/step_models.dart
+++ lib/models/step_models.dart
@@ -9,18 +9,18 @@ class StepModel {
     StepModel(
       id: 1,
       header: "Find a Salon nearby",
-      text: "Search for the closet salon \naround you and \nbook an appointment",
+      text: "Having problems locating a salon nearby?",
     ),
     StepModel(
       id: 2,
       header: '',
       text:
-          "Search for the closet salon \naround you and \nbook an appointment",
+          "Search for the closest salon \naround you and \nbook an appointment",
     ),
     StepModel(
       id: 3,
       header: "Professional Services",
-      text: "Search for the closet salon \naround you and \nbook an appointment",
+      text: "Great hair doesn't happen by chance, it happens by appointments!",
     ),
   ];
 }
